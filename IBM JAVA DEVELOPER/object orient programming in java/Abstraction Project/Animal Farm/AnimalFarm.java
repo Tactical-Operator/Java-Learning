4a2504710d855f7ebb300786f2b539471b7bf691
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-07-01 20:54:11 +0530

    modified AnimalFarm.java

--- IBM JAVA DEVELOPER/object orient programming in java/Abstraction Project/Animal Farm/AnimalFarm.java
+++ IBM JAVA DEVELOPER/object orient programming in java/Abstraction Project/Animal Farm/AnimalFarm.java
@@ -1,67 +1,98 @@
 import java.util.Scanner;
 
 public class AnimalFarm {
-    public static void main(String []args){
-        Scanner sc=new Scanner(System.in);
-        Animal[] animals=new Animal[4]; // array of references
-        int anmlIdx=0;
-        while(true){
-            System.out.println(  " Press 1 to view the animals "+
-                               "\n Press 2  to add animals "+
-                               "\n Press any other key to exit");
+    public static void main(String[] args) {
 
-            String userAction =sc.nextLine();
+        Scanner sc = new Scanner(System.in);
 
-            if(userAction.equals("1")){
-                for(int i=0;i<animals.length;i++){
-                    if(animals[i]!=null){
+        Animal[] animals = new Animal[4]; // array of references
+        int anmlIdx = 0;
+
+        while (true) {
+
+            System.out.println(
+                    "Press 1 to view the animals" +
+                    "\nPress 2 to add animals" +
+                    "\nPress any other key to exit");
+
+            String userAction = sc.nextLine();
+
+            if (userAction.equals("1")) {
+
+                for (int i = 0; i < animals.length; i++) {
+                    if (animals[i] != null) {
                         System.out.println(animals[i]);
                     }
                 }
-            }
-            else if(userAction.equals("2")){
-                if(anmlIdx==4){
-                    System.out.println("4 animals added already, cannot addmore animals");
+
+            } else if (userAction.equals("2")) {
+
+                if (anmlIdx == animals.length) {
+                    System.out.println("4 animals added already, cannot add more animals.");
                     continue;
                 }
-                System.out.println("Which animal do you want to create? \n Press 1 for dog"+
-                                      "\n Press 2 for cat"+"\n Press 3 for cat");
-                                     
-                String animalchoice=sc.nextLine();
 
-                if((animalchoice.equals("1"))){
+                System.out.println(
+                        "Which animal do you want to create?" +
+                        "\nPress 1 for Dog" +
+                        "\nPress 2 for Cat" +
+                        "\nPress 3 for Cow");
+
+                String animalChoice = sc.nextLine();
+
+                if (animalChoice.equals("1")) {
+
                     System.out.println("Enter the name of the dog");
-                    String dogName=sc.nextLine();
-                    // Animal-"I'll treat is like Animal" anmltmp can point to any object that is a animal
-                    Animal anmlTmp=new Dog(dogName); // creates a real dog object in heap memory
+                    String dogName = sc.nextLine();
+
+                    Animal anmlTmp = new Dog(dogName);
+
                     System.out.println("Enter what the dog eats");
-                    String dogFood=sc.nextLine();
+                    String dogFood = sc.nextLine();
+
                     anmlTmp.setFood(dogFood);
-                    animals[anmlIdx++]=anmlTmp;
-                }
 
-                else if(animalchoice.equals("2")){
+                    animals[anmlIdx++] = anmlTmp;
+
+                } else if (animalChoice.equals("2")) {
+
                     System.out.println("Enter the name of the cat");
-                    String catName=sc.nextLine();
-                    Animal anmlTmp=new Cat(catName);
+                    String catName = sc.nextLine();
+
+                    Animal anmlTmp = new Cat(catName);
+
                     System.out.println("Enter what the cat eats");
-                    String catFood=sc.nextLine();
+                    String catFood = sc.nextLine();
+
                     anmlTmp.setFood(catFood);
-                    animals[anmlIdx++]=anmlTmp;
-                }
 
-                else if(animalchoice.equals("3")){
+                    animals[anmlIdx++] = anmlTmp;
+
+                } else if (animalChoice.equals("3")) {
+
                     System.out.println("Enter the name of the cow");
-                    String cowName=sc.nextLine();
-                    Animal anmlTmp=new Cow(cowName);
+                    String cowName = sc.nextLine();
+
+                    Animal anmlTmp = new Cow(cowName);
+
                     System.out.println("What does the cow eat");
-                    String cowFood=sc.nextLine();
-                    anmlTmp.setFood(cowFood);   
-                    animals[anmlIdx++]=anmlTmp;
+                    String cowFood = sc.nextLine();
+
+                    anmlTmp.setFood(cowFood);
+
+                    animals[anmlIdx++] = anmlTmp;
+
+                } else {
+                    System.out.println("Invalid animal choice.");
                 }
 
-                
+            } else {
+                System.out.println("Exiting program...");
+                break; // Exit the while loop
             }
         }
+
+        // Scanner is closed only after the loop finishes
+        sc.close();
     }
-} 
+}
