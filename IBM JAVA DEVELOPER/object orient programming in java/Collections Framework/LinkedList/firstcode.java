023d8d1180d287121d13de93ce137d0e82f80b8e
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-07-23 22:05:35 +0530

    LinkedList folder added

---
+++ IBM JAVA DEVELOPER/object orient programming in java/Collections Framework/LinkedList/firstcode.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+import java.util.LinkedList;
+class  firstcode 
+{
+    public static void main(String[] args)
+    {
+       LinkedList<String> fruits=new LinkedList<>();
+       fruits.add("Mango");
+       fruits.add("Apple");
+       System.out.println(fruits.get(1));
+
+    }
+}
