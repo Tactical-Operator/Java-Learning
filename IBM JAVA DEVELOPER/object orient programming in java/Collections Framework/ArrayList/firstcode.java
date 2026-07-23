ccb7a126d063b6781313dc20ed82f221d2124698
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-07-23 20:08:08 +0530

    Array List folder added

---
+++ IBM JAVA DEVELOPER/object orient programming in java/Collections Framework/ArrayList/firstcode.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.List;
+
+class firstcode{
+
+    public static void main(String []args){
+
+        List<String> fruits=new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Mango");
+
+        System.out.println(fruits.get(1));
+    }
+}
