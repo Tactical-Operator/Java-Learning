a62a2be9c71af5bd0aad59911c84dfcd41bac5bb
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-07-29 21:35:27 +0530

    updated code in LinkedList

--- IBM JAVA DEVELOPER/object orient programming in java/Collections Framework/Lists/LinkedList/firstcode.java
+++ IBM JAVA DEVELOPER/object orient programming in java/Collections Framework/Lists/LinkedList/firstcode.java
@@ -6,7 +6,9 @@ class  firstcode
        LinkedList<String> fruits=new LinkedList<>();
        fruits.add("Mango");
        fruits.add("Apple");
+       fruits.add("Banana");
        System.out.println(fruits.get(1));
+       System.out.println();
        System.out.println(fruits);
 
     }
