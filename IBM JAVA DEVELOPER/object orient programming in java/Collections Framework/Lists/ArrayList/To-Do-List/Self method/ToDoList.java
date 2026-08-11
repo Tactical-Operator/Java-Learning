e8e857b2b621f3805887a5dc121e241923df750d
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-08-11 21:21:19 +0530

    updated code

---
+++ IBM JAVA DEVELOPER/object orient programming in java/Collections Framework/Lists/ArrayList/To-Do-List/Self method/ToDoList.java
@@ -0,0 +1,15 @@
+// to store the tasks created
+
+import java.util.*;
+public class ToDoList{
+
+    private ArrayList<Task> tasks;
+
+    // constructor to create a new arraylist
+    // when a ToDoList is created :
+    //                             create an empty ArrayList
+    public ToDoList(){
+        tasks = new ArrayList<>();
+    }
+    
+}
