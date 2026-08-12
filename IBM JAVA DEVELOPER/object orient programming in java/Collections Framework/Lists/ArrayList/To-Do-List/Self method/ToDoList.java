f3d53b2e8a7b65064341bcc366b464efdf243ee4
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-08-12 11:56:31 +0530

    completed code

--- IBM JAVA DEVELOPER/object orient programming in java/Collections Framework/Lists/ArrayList/To-Do-List/Self method/ToDoList.java
+++ IBM JAVA DEVELOPER/object orient programming in java/Collections Framework/Lists/ArrayList/To-Do-List/Self method/ToDoList.java
@@ -1,15 +1,35 @@
 // to store the tasks created
 
 import java.util.*;
-public class ToDoList{
+
+public class ToDoList {
 
     private ArrayList<Task> tasks;
 
     // constructor to create a new arraylist
     // when a ToDoList is created :
-    //                             create an empty ArrayList
-    public ToDoList(){
+    // create an empty ArrayList
+    public ToDoList() {
         tasks = new ArrayList<>();
     }
-    
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void displayTasks() {
+
+        for (Task task : tasks) {
+
+            System.out.println(task.get_id());
+            System.out.println(task.get_task_title());
+            System.out.println(task.get_priority());
+            System.out.println(task.get_status());
+        }
+    }
+
 }
+// at this point ToDoList can :
+// 1. create an empty list
+// 2. Accept Task Objects
+// 3. Store those Task Objects
