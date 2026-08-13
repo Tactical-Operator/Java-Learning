4c60edb25c46baf1224c336afc628934e4735c4d
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-08-13 21:53:23 +0530

    added markTaskCompleted() function to it

--- IBM JAVA DEVELOPER/object orient programming in java/Collections Framework/Lists/ArrayList/To-Do-List/Self method/ToDoList.java
+++ IBM JAVA DEVELOPER/object orient programming in java/Collections Framework/Lists/ArrayList/To-Do-List/Self method/ToDoList.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public  class ToDoList {
 
-    private ArrayList<Task> tasks;
+     ArrayList<Task> tasks;
 
     // constructor to create a new arraylist
     // when a ToDoList is created :
@@ -21,12 +21,31 @@ public  class ToDoList {
 
         for (Task task : tasks) {
 
-            System.out.println(task.get_id());
-            System.out.println(task.get_task_title());
-            System.out.println(task.get_priority());
-            System.out.println(task.get_status());
-            System.out.println();
+           System.out.println(" _______________________________");
+           System.out.println("|                               |");
+           System.out.printf("|  ID = %-24s|%n", task.get_id());
+           System.out.printf("|  Title = %-21s|%n", task.get_task_title());
+           System.out.printf("|  Priority = %-18s|%n", task.get_priority());
+           System.out.printf("|  Status = %-20s|%n", task.get_status());
+           System.out.println("|_______________________________|");
+           System.out.println();
         }
+    }
+
+    public void markTaskCompleted(int id){
+
+        for(Task task:tasks){
+
+            if(task.get_id()==id)
+            {
+                task.markCompleted();
+
+                System.out.println("  Test marked as completed");
+
+                return;
+            }
+        }
+        System.out.println("  Task not Found");
     }
 
 }
