5538aae48e60a8c6a8111e151d5ffce8df116eb4
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-08-26 21:36:58 +0530

    new updates

--- IBM JAVA DEVELOPER/object orient programming in java/Date and Time/schedule_meetings_for_different_time_zones.java
+++ IBM JAVA DEVELOPER/object orient programming in java/Date and Time/schedule_meetings_for_different_time_zones.java
@@ -6,6 +6,7 @@ public class schedule_meetings_for_different_time_zones{
     public static void main(String[] args) {
         ZonedDateTime meetingTimeUTC = ZonedDateTime.parse("2026-08-25T23:00:00Z");
 
+
         String[] participantTimeZones = {
             "America/New_York",
             "Asia/Kolkata",
@@ -16,8 +17,9 @@ public class schedule_meetings_for_different_time_zones{
         System.out.println("Meeting time in utc: " + meetingTimeUTC.format(formatter));
 
         for(String timeZone : participantTimeZones){
-            ZonedDateTime LocalTime = meetingTimeUTC.withZoneSameInstant(ZoneId.of(timeZone));
+            ZonedDateTime LocalTime = meetingTimeUTC.withZoneSameInstant(ZoneId.of(timeZone));// converts to utc time 
             System.out.println("meeting time in "+ timeZone + ": " + LocalTime.format(formatter));
         }
     }
 }
+// no new update
